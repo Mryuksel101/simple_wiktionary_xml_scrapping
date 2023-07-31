1752135f14386366559b5f74c8ef417bd63356dc
mustafa <mryuksel5@gmail.com>
2023-07-31 23:42:33 +0300

    deneme

--- sozluk.py
+++ sozluk.py
@@ -1,3 +1,40 @@
+""" 
+{
+    word: ....
+    [
+        {   
+            des : ....
+            sentences :
+                [         
+                    
+                ]
+            type: .......
+        }
+    ]
+}
+
+{
+    word: ....
+    [
+        [
+            {   
+                des : ...
+                sentences : [
+                
+                ]
+            }
+        ]
+        type: .......
+    ]
+}
+
+for i, item in enumerate(noun_sections, 1):
+    print(f"== Noun {i} ==\n{item}\n")
+
+pattern = re.compile(r'== Noun ==(.+?)\n\n', re.DOTALL)
+noun_sections = pattern.findall(text)
+""" 
+
 import xml.etree.ElementTree as ET
 import re
 with open('dic.xml', 'r') as file:
@@ -7,14 +44,20 @@ with open('dic.xml', 'r') as file:
 tree = ET.ElementTree(ET.fromstring(xml_data))
 root = tree.getroot()
 
-searchedWord = root.find('title').text
-wordMeaningAll = root.find('.//text').text
-# 'Noun' bölümünü arıyoruz
-pattern = re.compile(r'== Noun ==(.+?)\n\n', re.DOTALL)
-noun_sections = pattern.findall(wordMeaningAll)
+word = root.find('title').text
+text = root.find('.//text').text
 
-for i, item in enumerate(noun_sections, 1):
-    print(f"== Noun {i} ==\n{item}\n")
+# 'Noun' bölümünü
+desPattern = re.compile(r'#(.+?)\n', re.DOTALL)
+sentencesPattern = re.compile(r'#:(.+?)\n', re.DOTALL)
+des = desPattern.search(text).group(1)
+sentences = sentencesPattern.findall(text)
+
+for i in sentences:
+    print(i)
+
+
+
 
 
 
